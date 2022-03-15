a3b840409ffe7002f802f1fbd782e12f7af4f197
Arielle Domantay <domantayarielle@gmail.com>
2022-03-15 19:58:46 -0400

    started project

---
+++ WerddApp-ArielleDOM/WerddApp-ArielleDOM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WerddApp-ArielleDOM
+//
+//  Created by Arielle Domantay on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
